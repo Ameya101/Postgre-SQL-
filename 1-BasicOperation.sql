b0373c12f49363f955df007bb1872fef861cf9a2
Ameya Jajulwar <66177442+Ameya101@users.noreply.github.com>
2021-05-04 03:21:16 +0530

    Add files via upload

---
+++ 1-BasicOperation.sql
@@ -0,0 +1,45 @@
+-- Basic Commands
+CREATE DATABASE shubham;
+DROP DATABASE shubham;
+USE shubham;
+CREATE SCHEMA shubham;
+
+
+
+
+-- Create Table
+CREATE TABLE shubham.CUSTOMERS(
+   ID   INT              NOT NULL,
+   NAME VARCHAR (20)     NOT NULL,
+   AGE  INT              NOT NULL,
+   ADDRESS  CHAR (25) ,
+   SALARY   DECIMAL (18, 2),       
+   PRIMARY KEY (ID)
+);
+
+
+-- For Learning JOINS
+CREATE TABLE basket_a (
+    id INT PRIMARY KEY,
+    fruit VARCHAR (100) NOT NULL
+);
+ 
+CREATE TABLE basket_b (
+    id INT PRIMARY KEY,
+    fruit VARCHAR (100) NOT NULL
+);
+ 
+INSERT INTO basket_a (id, fruit)
+VALUES
+    (1, 'Apple'),
+    (2, 'Orange'),
+    (3, 'Banana'),
+    (4, 'Cucumber');
+ 
+INSERT INTO basket_b (id, fruit)
+VALUES
+    (1, 'Orange'),
+    (2, 'Apple'),
+    (3, 'Watermelon'),
+    (4, 'Pear');
+
